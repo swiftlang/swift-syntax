61a36b4433b11df60ccc947242f20fa494e8ed07
Xi Ge <xi_ge@apple.com>
2018-11-04 08:40:51 -0800

    Revert "Add a dynamic and static library product of SwiftSyntax to Package.swift"

--- Package.swift
+++ Package.swift
@@ -4,13 +4,21 @@ import PackageDescription
 
 let package = Package(
   name: "SwiftSyntax",
-  products: [
-    .library(name: "SwiftSyntax", targets: ["SwiftSyntax"]),
-    .library(name: "SwiftSyntax-dynamic", type: .dynamic, targets: ["SwiftSyntax"]),
-  ],
   targets: [
     .target(name: "SwiftSyntax"),
     .testTarget(name: "SwiftSyntaxTest", dependencies: ["SwiftSyntax"], exclude: ["Inputs"]),
     .target(name: "lit-test-helper", dependencies: ["SwiftSyntax"])
   ]
 )
+
+#if os(Linux)
+import Glibc
+#else
+import Darwin.C
+#endif
+
+if getenv("SWIFT_SYNTAX_BUILD_SCRIPT") == nil {
+  package.products.append(.library(name: "SwiftSyntax", targets: ["SwiftSyntax"]))
+} else {
+  package.products.append(.library(name: "SwiftSyntax", type: .dynamic, targets: ["SwiftSyntax"]))
+}

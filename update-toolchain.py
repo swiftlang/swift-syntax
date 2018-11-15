76fe3904e567a36b0129203181b87d52e1fce594
Ross Bayer <rbayer@apple.com>
2018-11-14 18:18:08 -0800

    Updated the update-toolchain.py script to conform to the PEP8 formatting standard.

--- update-toolchain.py
+++ update-toolchain.py
@@ -1,78 +1,135 @@
 #!/usr/bin/env python
-from __future__ import print_function
+
+
+from __future__ import absolute_import, print_function, unicode_literals
 
 import argparse
 import os
 import subprocess
 import sys
 import tempfile
-import errno
-import platform
-import shutil
 
-BUILD_SCRIPT = os.path.dirname(os.path.abspath(__file__)) + '/build-script.py'
 
-def printerr(message):
-    print(message, file=sys.stderr)
+try:
+    # Python 2
+    from pipes import quote
+except ImportError:
+    from shutil import quote
 
-def note(message):
-    print("--- %s: note: %s" % (os.path.basename(sys.argv[0]), message))
-    sys.stdout.flush()
 
-def fatal_error(message):
-    printerr(message)
-    sys.exit(1)
+# -----------------------------------------------------------------------------
+# Constants
 
-def escapeCmdArg(arg):
-    if '"' in arg or ' ' in arg:
-        return '"%s"' % arg.replace('"', '\\"')
-    else:
-        return arg
+PACKAGE_DIR = os.path.abspath(os.path.dirname(__file__))
+BUILD_SCRIPT = os.path.join(PACKAGE_DIR, 'build-script.py')
 
-def check_call(cmd, env=os.environ):
-    print(' '.join([escapeCmdArg(arg) for arg in cmd]))
-    return subprocess.check_call(cmd, env=env, stderr=subprocess.STDOUT)
-
-def build(swiftc_exec, build_dir):
-    check_call([BUILD_SCRIPT, '--release', '--swiftc-exec', swiftc_exec,
-        '--build-dir', build_dir, '-v'])
-
-def install(swiftc_exec, build_dir, dylib_dir, module_dir):
-    check_call([BUILD_SCRIPT, '--release', '--install', '--swiftc-exec',
-        swiftc_exec, '--dylib-dir', dylib_dir, '--swiftmodule-dir', module_dir,
-        '--build-dir', build_dir, '-v'])
-
-def main():
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description='''
+ARGUMENT_PARSER_DESCRIPTION = """
 Build and re-install SwiftSytnax for an existing toolchain.
 
 This script calls ./build-script internally to rebuild SwiftSyntax using an
 existing toolchain and install the build artifacts back to the toolchain.
-''')
+"""
 
-    basic_group = parser.add_argument_group('Basic')
 
-    basic_group.add_argument('--toolchain-dir', default=None, help='''
-        Directory of an existing toolchain.
-        ''')
+# -----------------------------------------------------------------------------
+# Helpers
 
-    args = parser.parse_args(sys.argv[1:])
-    if not args.toolchain_dir:
-        fatal_error("Need to specify --toolchain-dir")
+def _print(*objects, **kwargs):
+    """Simple backport of the Python 3 print with flush support.
+    """
+
+    # Keyword only arguments
+    sep = kwargs.get('sep', ' ')
+    end = kwargs.get('end', '\n')
+    file = kwargs.get('file', sys.stdout)
+    flush = kwargs.get('flush', False)
+
+    file.write(sep.join(objects) + end)
+
+    if flush:
+        file.flush()
+
+
+def info(*objects):
+    _print(*objects, file=sys.stdout)
+
+
+def error(*objects):
+    _print('ERROR:', *objects, file=sys.stderr)
+
+
+def fatal_error(*objects):
+    error(*objects)
+    sys.exit(1)
+
+
+def quote_command(command):
+    return ' '.join([quote(arg) for arg in command])
+
+
+# -----------------------------------------------------------------------------
+# Build and Install
+
+def build(swiftc_exec, build_dir):
+    command = [
+        BUILD_SCRIPT, '-v',
+        '--release',
+        '--build-dir', build_dir,
+        '--swiftc-exec', swiftc_exec,
+    ]
+
+    info(quote_command(command))
+    subprocess.check_call(command)
+
+
+def install(swiftc_exec, build_dir, dylib_dir, module_dir):
+    command = [
+        BUILD_SCRIPT, '-v',
+        '--release',
+        '--install',
+        '--build-dir', build_dir,
+        '--dylib-dir', dylib_dir,
+        '--swiftc-exec', swiftc_exec,
+        '--swiftmodule-dir', module_dir,
+    ]
+
+    info(quote_command(command))
+    subprocess.check_call(command)
+
+
+# -----------------------------------------------------------------------------
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=ARGUMENT_PARSER_DESCRIPTION)
+
+    parser.add_argument('--toolchain-dir',
+                        required=True,
+                        help='Directory of an existing toolchain.')
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
     if not os.geteuid() == 0:
-        fatal_error("\n!!!!!!!!Run this script with root access!!!!!!!!\n")
+        fatal_error('Re-run this script with root access!')
 
     build_dir = tempfile.mkdtemp()
-    swiftc_exec = args.toolchain_dir + '/usr/bin/swiftc'
-    dylib_dir = args.toolchain_dir + '/usr/lib/swift/macosx'
-    module_dir = args.toolchain_dir + '/usr/lib/swift/macosx/x86_64'
-    print('Using swiftc: ' + swiftc_exec)
-    print('Using build directory: ' + build_dir)
-    build(swiftc_exec=swiftc_exec, build_dir=build_dir)
-    install(swiftc_exec=swiftc_exec, build_dir=build_dir, dylib_dir=dylib_dir,
-        module_dir=module_dir)
+    toolchain_usr_dir = os.path.join(args.toolchain_dir, 'usr')
+
+    swiftc_exec = os.path.join(toolchain_usr_dir, 'bin', 'swiftc')
+    dylib_dir = os.path.join(toolchain_usr_dir, 'lib', 'swift', 'macosx')
+    module_dir = os.path.join(dylib_dir, 'x86_64')
+
+    info('Using swiftc:', swiftc_exec)
+    info('Using build directory:', build_dir)
+
+    build(swiftc_exec, build_dir)
+    install(swiftc_exec, build_dir, dylib_dir, module_dir)
+
 
 if __name__ == '__main__':
     main()

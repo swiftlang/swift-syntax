b3bf615885bf620d28117dd5e827f5da6b83e5f0
Kuniwak <orga.chem.job@gmail.com>
2019-03-01 23:28:07 +0900

    Add tests for dumped results of Syntax

--- Tests/SwiftSyntaxTest/CustomReflecatbleTests.swift
+++ Tests/SwiftSyntaxTest/CustomReflecatbleTests.swift
@@ -4,8 +4,216 @@ import SwiftSyntax
 
 public class CustomReflectableTests: XCTestCase {
   public static let allTests = [
+    ("testDump", testDump),
     ("testConformanceToCustomReflectable", testConformanceToCustomReflectable),
   ]
+
+
+  public func testDump() {
+    struct TestCase {
+      let syntax: Any
+      let expectedDumped: String
+    }
+
+    let testCases: [UInt: TestCase] = [
+      #line: .init(syntax: SyntaxFactory.makeUnknownSyntax(tokens: []),
+                   expectedDumped: """
+                                   - UnknownSyntax
+
+                                   """),
+      #line: .init(syntax: SyntaxFactory.makeToken(.associatedtypeKeyword,
+                                                   presence: .present,
+                                                   leadingTrivia: [],
+                                                   trailingTrivia: []),
+                   expectedDumped: """
+                                   ▿ TokenSyntax
+                                     - text: "associatedtype"
+                                     ▿ leadingTrivia: SwiftSyntax.Trivia
+                                       - pieces: 0 elements
+                                     ▿ trailingTrivia: SwiftSyntax.Trivia
+                                       - pieces: 0 elements
+                                     - tokenKind: SwiftSyntax.TokenKind.associatedtypeKeyword
+
+                                   """),
+      #line: {
+        let leftToken = SyntaxFactory.makeToken(.leftSquareBracket,
+                                                presence: .present,
+                                                leadingTrivia: [],
+                                                trailingTrivia: [])
+        let elements = SyntaxFactory.makeBlankArrayElementList()
+        let rightToken = SyntaxFactory.makeToken(.rightSquareBracket,
+                                                 presence: .present,
+                                                 leadingTrivia: [],
+                                                 trailingTrivia: [])
+        let expr = SyntaxFactory.makeArrayExpr(leftSquare: leftToken,
+                                               elements: elements,
+                                               rightSquare: rightToken)
+        return .init(syntax: expr.tokens,
+                     expectedDumped: """
+                                     ▿ SwiftSyntax.TokenSequence
+                                       ▿ TokenSyntax
+                                         - text: "["
+                                         ▿ leadingTrivia: SwiftSyntax.Trivia
+                                           - pieces: 0 elements
+                                         ▿ trailingTrivia: SwiftSyntax.Trivia
+                                           - pieces: 0 elements
+                                         - tokenKind: SwiftSyntax.TokenKind.leftSquareBracket
+                                       ▿ TokenSyntax
+                                         - text: "]"
+                                         ▿ leadingTrivia: SwiftSyntax.Trivia
+                                           - pieces: 0 elements
+                                         ▿ trailingTrivia: SwiftSyntax.Trivia
+                                           - pieces: 0 elements
+                                         - tokenKind: SwiftSyntax.TokenKind.rightSquareBracket
+
+                                     """)
+      }(),
+      #line: {
+        let leftToken = SyntaxFactory.makeToken(.leftSquareBracket,
+                                                presence: .present,
+                                                leadingTrivia: [],
+                                                trailingTrivia: [])
+        let elements = SyntaxFactory.makeBlankArrayElementList()
+        let rightToken = SyntaxFactory.makeToken(.rightSquareBracket,
+                                                 presence: .present,
+                                                 leadingTrivia: [],
+                                                 trailingTrivia: [])
+        let expr = SyntaxFactory.makeArrayExpr(leftSquare: leftToken,
+                                               elements: elements,
+                                               rightSquare: rightToken)
+        return .init(syntax: expr.tokens.reversed(),
+                     expectedDumped: """
+                                     ▿ SwiftSyntax.ReversedTokenSequence
+                                       ▿ TokenSyntax
+                                         - text: "]"
+                                         ▿ leadingTrivia: SwiftSyntax.Trivia
+                                           - pieces: 0 elements
+                                         ▿ trailingTrivia: SwiftSyntax.Trivia
+                                           - pieces: 0 elements
+                                         - tokenKind: SwiftSyntax.TokenKind.rightSquareBracket
+                                       ▿ TokenSyntax
+                                         - text: "["
+                                         ▿ leadingTrivia: SwiftSyntax.Trivia
+                                           - pieces: 0 elements
+                                         ▿ trailingTrivia: SwiftSyntax.Trivia
+                                           - pieces: 0 elements
+                                         - tokenKind: SwiftSyntax.TokenKind.leftSquareBracket
+
+                                     """)
+      }(),
+      #line: {
+        let token1 = SyntaxFactory.makeToken(.integerLiteral("1"),
+                                             presence: .present,
+                                             leadingTrivia: [],
+                                             trailingTrivia: [])
+        let expr1 = SyntaxFactory.makeIntegerLiteralExpr(digits: token1)
+        let token2 = SyntaxFactory.makeToken(.integerLiteral("2"),
+                                             presence: .present,
+                                             leadingTrivia: [],
+                                             trailingTrivia: [])
+        let expr2 = SyntaxFactory.makeIntegerLiteralExpr(digits: token2)
+        let elements = [SyntaxFactory.makeTupleElement(label: nil,
+                                                       colon: nil,
+                                                       expression: expr1,
+                                                       trailingComma: nil),
+                        SyntaxFactory.makeTupleElement(label: nil,
+                                                       colon: nil,
+                                                       expression: expr2,
+                                                       trailingComma: nil)]
+        let tuples = SyntaxFactory.makeTupleElementList(elements)
+        return .init(syntax: tuples,
+                     expectedDumped: """
+                                     ▿ TupleElementListSyntax
+                                       ▿ TupleElementSyntax
+                                         - label: nil
+                                         - colon: nil
+                                         ▿ expression: IntegerLiteralExprSyntax
+                                           ▿ digits: TokenSyntax
+                                             - text: "1"
+                                             ▿ leadingTrivia: SwiftSyntax.Trivia
+                                               - pieces: 0 elements
+                                             ▿ trailingTrivia: SwiftSyntax.Trivia
+                                               - pieces: 0 elements
+                                             ▿ tokenKind: SwiftSyntax.TokenKind.integerLiteral
+                                               - integerLiteral: "1"
+                                         - trailingComma: nil
+                                       ▿ TupleElementSyntax
+                                         - label: nil
+                                         - colon: nil
+                                         ▿ expression: IntegerLiteralExprSyntax
+                                           ▿ digits: TokenSyntax
+                                             - text: "2"
+                                             ▿ leadingTrivia: SwiftSyntax.Trivia
+                                               - pieces: 0 elements
+                                             ▿ trailingTrivia: SwiftSyntax.Trivia
+                                               - pieces: 0 elements
+                                             ▿ tokenKind: SwiftSyntax.TokenKind.integerLiteral
+                                               - integerLiteral: "2"
+                                         - trailingComma: nil
+
+                                     """)
+      }(),
+      #line: {
+        let token1 = SyntaxFactory.makeToken(.integerLiteral("1"),
+                                             presence: .present,
+                                             leadingTrivia: [],
+                                             trailingTrivia: [])
+        let expr1 = SyntaxFactory.makeIntegerLiteralExpr(digits: token1)
+        let token2 = SyntaxFactory.makeToken(.integerLiteral("2"),
+                                             presence: .present,
+                                             leadingTrivia: [],
+                                             trailingTrivia: [])
+        let expr2 = SyntaxFactory.makeIntegerLiteralExpr(digits: token2)
+        let elements = [SyntaxFactory.makeTupleElement(label: nil,
+                                                       colon: nil,
+                                                       expression: expr1,
+                                                       trailingComma: nil),
+          SyntaxFactory.makeTupleElement(label: nil,
+                                         colon: nil,
+                                         expression: expr2,
+                                         trailingComma: nil)]
+        let tuples = SyntaxFactory.makeTupleElementList(elements)
+        return .init(syntax: tuples.reversed(),
+                     expectedDumped: """
+                                     ▿ SwiftSyntax.TupleElementListSyntax.Reversed
+                                       ▿ collection: TupleElementListSyntax
+                                         ▿ TupleElementSyntax
+                                           - label: nil
+                                           - colon: nil
+                                           ▿ expression: IntegerLiteralExprSyntax
+                                             ▿ digits: TokenSyntax
+                                               - text: "1"
+                                               ▿ leadingTrivia: SwiftSyntax.Trivia
+                                                 - pieces: 0 elements
+                                               ▿ trailingTrivia: SwiftSyntax.Trivia
+                                                 - pieces: 0 elements
+                                               ▿ tokenKind: SwiftSyntax.TokenKind.integerLiteral
+                                                 - integerLiteral: "1"
+                                           - trailingComma: nil
+                                         ▿ TupleElementSyntax
+                                           - label: nil
+                                           - colon: nil
+                                           ▿ expression: IntegerLiteralExprSyntax
+                                             ▿ digits: TokenSyntax
+                                               - text: "2"
+                                               ▿ leadingTrivia: SwiftSyntax.Trivia
+                                                 - pieces: 0 elements
+                                               ▿ trailingTrivia: SwiftSyntax.Trivia
+                                                 - pieces: 0 elements
+                                               ▿ tokenKind: SwiftSyntax.TokenKind.integerLiteral
+                                                 - integerLiteral: "2"
+                                           - trailingComma: nil
+
+                                     """)
+      }(),
+    ]
+
+    testCases.forEach { keyAndValue in
+      let (key: line, value: testCase) = keyAndValue
+      let actualDumped = dumped(testCase.syntax)
+      XCTAssertEqual(testCase.expectedDumped, actualDumped, line: line)
+    }
+  }
 
 
   public func testConformanceToCustomReflectable() {
@@ -48,5 +256,12 @@ public class CustomReflectableTests: XCTestCase {
 
       collectSyntaxNotConformedCustomReflectable(from: value, ancestors: currentPathComponents, foundPaths: &foundPaths)
     }
+  }
+
+
+  public func dumped(_ syntax: Any) -> String {
+    var result = ""
+    dump(syntax, to: &result)
+    return result
   }
 }

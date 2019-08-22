efd4dc80747d33f92f95b0f7dafa1aef84871f63
Tony Allevato <allevato@google.com>
2019-08-22 14:58:27 -0700

    Import _CSwiftSyntax using @_implementationOnly.
    This lets us add an actual header for the C function it defines,
    so that Swift can import it directly (while still retaining the
    property that it is a standalone module) instead of relying on
    the dlsym trick that fails on Linux unless additional flags are
    passed to the linker for any binary that depends on SwiftSyntax
    (see https://bugs.swift.org/browse/SR-11293).

--- Sources/SwiftSyntax/AtomicCounter.swift
+++ Sources/SwiftSyntax/AtomicCounter.swift
@@ -10,36 +10,12 @@
 //
 //===----------------------------------------------------------------------===//
 
-// This is using the C function from '_CSwiftSyntax/src/atomic-counter.c' by
-// dynamically loading it using `dlsym`. The reason this mechanism is used
-// instead of importing a header is so that we preserve the property that
-// SwiftSyntax is a self-contained Swift module.
-// (also see '_CSwiftSyntax/include/README.md')
-
-#if os(Linux)
-import Glibc
-#else
-import Darwin.C
-#endif
-
-#if os(macOS) || os(iOS) || os(watchOS) || os(tvOS)
-  fileprivate let RTLD_DEFAULT = UnsafeMutableRawPointer(bitPattern: -2)
-#elseif os(Linux)
-  fileprivate let RTLD_DEFAULT = UnsafeMutableRawPointer(bitPattern: 0)
-#endif
-
-fileprivate typealias get_unique_counter_ty = (@convention(c) ()->UInt64)
-fileprivate let get_unique_counter_str = "cswiftsyntax_get_unique_counter"
+@_implementationOnly import _CSwiftSyntax
 
 /// Provides API to get an atomically increasing counter value.
 struct AtomicCounter {
   /// Get an atomically increasing counter value.
   static func next() -> UInt64 {
-    return AtomicCounter.cswiftsyntax_get_unique_counter()
+    return cswiftsyntax_get_unique_counter()
   }
-
-  static fileprivate let cswiftsyntax_get_unique_counter: get_unique_counter_ty = { ()->get_unique_counter_ty in
-    let ptr = dlsym(RTLD_DEFAULT, get_unique_counter_str)!
-    return unsafeBitCast(ptr, to: get_unique_counter_ty.self)
-  }()
 }

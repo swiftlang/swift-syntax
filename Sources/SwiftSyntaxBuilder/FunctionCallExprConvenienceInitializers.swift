c1b300763b88b8b6c7b39961b0339a64a907b417
Kim de Vos <kimdevos12@hotmail.com>
2021-10-27 21:17:47 +0200

    Add convenience initializers function call expr

---
+++ Sources/SwiftSyntaxBuilder/FunctionCallExprConvenienceInitializers.swift
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2021 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+
+import Foundation
+
+extension FunctionCallExpr {
+  public init(
+    _ calledExpression: IdentifierExpr,
+    leftParen: ExpressibleAsTokenSyntax? = nil,
+    rightParen: ExpressibleAsTokenSyntax? = nil,
+    trailingClosure: ExpressibleAsClosureExpr? = nil,
+    @TupleExprElementListBuilder argumentListBuilder: () -> TupleExprElementList = { .empty },
+    @MultipleTrailingClosureElementListBuilder additionalTrailingClosuresBuilder: () -> MultipleTrailingClosureElementList? = { nil }
+  ) {
+    self.init(
+      calledExpression: calledExpression,
+      leftParen: leftParen,
+      argumentList: argumentListBuilder(),
+      rightParen: rightParen,
+      trailingClosure: trailingClosure,
+      additionalTrailingClosures: additionalTrailingClosuresBuilder()
+    )
+  }
+}

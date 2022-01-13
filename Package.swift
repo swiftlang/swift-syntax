d12bfd955d288ef7373f3a066f7519f0746b9144
Alex Hoppen <ahoppen@apple.com>
2022-01-13 16:02:59 +0100

    Include Swift 5.5’s `_InternalSwiftSyntaxParser.dylib` in the repo
    Previously, clients of SwiftSyntax always needed to provide a `_InternalSwiftSyntaxParser.dylib` that was matching the SwiftSyntax version they are using. This was fragile and annoying.
    
    Add the `_InternalSwiftSyntaxParser.dylib` that was released as part of Swift 5.5 to the repo as a binary framework dependency. This way, users can just depend on the SwiftSyntax package and it will bring along the `_InternalSwiftSyntaxParser.dylib` it needs.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.3
 
 import PackageDescription
 import Foundation
@@ -6,17 +6,36 @@ import Foundation
 let package = Package(
   name: "SwiftSyntax",
   targets: [
-    .target(name: "_CSwiftSyntax"),
+    .target(name: "_CSwiftSyntax", exclude: ["README.md"]),
     .testTarget(name: "SwiftSyntaxTest", dependencies: ["SwiftSyntax"], exclude: ["Inputs"]),
-    .target(name: "SwiftSyntaxBuilder", dependencies: ["SwiftSyntax"]),
+    .target(name: "SwiftSyntaxBuilder", dependencies: ["SwiftSyntax"], exclude: ["README.md", "Tokens.swift.gyb"]),
     .testTarget(name: "SwiftSyntaxBuilderTest", dependencies: ["SwiftSyntaxBuilder"]),
     .target(name: "lit-test-helper", dependencies: ["SwiftSyntax"]),
-    .testTarget(name: "PerformanceTest", dependencies: ["SwiftSyntax"])
+    .testTarget(name: "PerformanceTest", dependencies: ["SwiftSyntax"], exclude: ["Inputs"]),
+    .binaryTarget(name: "_InternalSwiftSyntaxParser", url: "https://github.com/ahoppen/swift-syntax/releases/download/ahoppen-0.50500.2/_InternalSwiftSyntaxParser.xcframework.zip", checksum: "f1326cfbaee9924ba925c33a3c85fd326cdc2771f68c23653b7cd8d520d0afd4"),
     // Also see targets added below
   ]
 )
 
 let swiftSyntaxTarget: PackageDescription.Target
+
+let swiftSyntaxGybSources = [
+  "SyntaxCollections.swift.gyb",
+  "TokenKind.swift.gyb",
+  "SyntaxBuilders.swift.gyb",
+  "Trivia.swift.gyb",
+  "SyntaxKind.swift.gyb",
+  "SyntaxBaseNodes.swift.gyb",
+  "SyntaxFactory.swift.gyb",
+  "SyntaxEnum.swift.gyb",
+  "SyntaxRewriter.swift.gyb",
+  "SyntaxTraits.swift.gyb",
+  "Misc.swift.gyb",
+  "SyntaxClassification.swift.gyb",
+  "SyntaxNodes.swift.gyb.template",
+  "SyntaxAnyVisitor.swift.gyb",
+  "SyntaxVisitor.swift.gyb",
+]
 
 /// If we are in a controlled CI environment, we can use internal compiler flags
 /// to speed up the build or improve it.
@@ -32,11 +51,11 @@ if ProcessInfo.processInfo.environment["SWIFT_BUILD_SCRIPT_ENVIRONMENT"] != nil 
   // Disable it when we're in a controlled CI environment.
   swiftSyntaxUnsafeFlags += ["-enforce-exclusivity=unchecked"]
 
-  swiftSyntaxTarget = .target(name: "SwiftSyntax", dependencies: ["_CSwiftSyntax"],
+  swiftSyntaxTarget = .target(name: "SwiftSyntax", dependencies: ["_CSwiftSyntax", "_InternalSwiftSyntaxParser"], exclude: swiftSyntaxGybSources,
                               swiftSettings: [.unsafeFlags(swiftSyntaxUnsafeFlags)]
   )
 } else {
-  swiftSyntaxTarget = .target(name: "SwiftSyntax", dependencies: ["_CSwiftSyntax"])
+  swiftSyntaxTarget = .target(name: "SwiftSyntax", dependencies: ["_CSwiftSyntax", "_InternalSwiftSyntaxParser"], exclude: swiftSyntaxGybSources)
 }
 
 package.targets.append(swiftSyntaxTarget)

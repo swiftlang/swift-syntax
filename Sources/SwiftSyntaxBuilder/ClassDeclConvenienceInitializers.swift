29193dcebc8c14eb1123fdc452098a75f0250534
Kim de Vos <kimdevos12@hotmail.com>
2022-02-17 22:13:43 +0100

    Add `SourceFile` tests

---
+++ Sources/SwiftSyntaxBuilder/ClassDeclConvenienceInitializers.swift
@@ -0,0 +1,38 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftSyntax
+
+extension ClassDecl {
+  /// A convenience initializer that allows passing in members using a result builder instead of having to wrap them in a `MemberDeclBlock`.
+  public init(
+    classOrActorKeyword: TokenSyntax,
+    identifier: String,
+    genericParameterClause: ExpressibleAsGenericParameterClause? = nil,
+    inheritanceClause: ExpressibleAsTypeInheritanceClause? = nil,
+    genericWhereClause: ExpressibleAsGenericWhereClause? = nil,
+    @AttributeListBuilder attributesBuilder: () -> ExpressibleAsAttributeList? = { nil },
+    @ModifierListBuilder modifiersBuilder: () -> ExpressibleAsModifierList? = { nil },
+    @MemberDeclListBuilder membersBuilder: () -> ExpressibleAsMemberDeclList = { MemberDeclList([]) }
+  ) {
+    self.init(
+      attributes: attributesBuilder(),
+      modifiers: modifiersBuilder(),
+      classOrActorKeyword: classOrActorKeyword,
+      identifier: TokenSyntax.identifier(identifier),
+      genericParameterClause: genericParameterClause,
+      inheritanceClause: inheritanceClause,
+      genericWhereClause: genericWhereClause,
+      members: membersBuilder()
+    )
+  }
+}

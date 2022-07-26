abfb866dc0b148c11b7bb0533c63cbe94f7c60bf
fwcd <fwcdmail@gmail.com>
2022-07-26 16:08:03 +0200

    Add convenience initializer for if-else statements

---
+++ Sources/SwiftSyntaxBuilder/IfStmtConvenienceInitializers.swift
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2022 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+import SwiftSyntax
+
+public extension IfStmt {
+  /// A convenience initializer that uses builder closures to express an
+  /// if body, potentially with a second trailing builder closure for an else
+  /// body.
+  init(
+    leadingTrivia: Trivia = [],
+    conditions: ExpressibleAsConditionElementList,
+    @CodeBlockItemListBuilder body: () -> ExpressibleAsCodeBlockItemList,
+    @CodeBlockItemListBuilder elseBody: () -> ExpressibleAsCodeBlockItemList? = { nil }
+  ) {
+    let generatedElseBody = elseBody()
+    self.init(
+      leadingTrivia: leadingTrivia,
+      conditions: conditions,
+      body: body(),
+      elseKeyword: generatedElseBody == nil ? nil : SyntaxFactory.makeElseKeyword(leadingTrivia: .space, trailingTrivia: []),
+      elseBody: generatedElseBody.map { CodeBlock(statements: $0) }
+    )
+  }
+}

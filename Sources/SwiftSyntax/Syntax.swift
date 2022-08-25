53d91e849e8b8ded3dafb299a28fad5dd47a9a15
Rintaro Ishizaki <rishizaki@apple.com>
2022-08-25 14:33:23 -0700

    Move AbsoluteSyntaxInfo to "edge" SyntaxData information
    The root node doesn't need to hold its `AbsoluteSyntaxInfo` because
    it's always zero. Move this information to "edge" data so only non-root
    nodes hold the "absolute info". Introduce an "edge" info struct, and use
    it with `indirect` enum case instead of `SyntaxBox`.
    
    Previously the structure of `SyntaxData` was:
     * AbsoluteRawSyntax
       * RawSyntax
       * AbsoluteSyntaxInfo
     * Tagged union of
       * SyntaxArena
       * SyntaxBox
         * Parent SyntaxData
    
    Now:
     * RawSyntax node
     * Tagged union of:
       * SyntaxData.Info.Root
         * SyntaxArena
       * SyntaxData.Info.Edge (indirect case)
         * Parent SyntaxData
         * AbsoluteSyntaxInfo
    
    This reduces the size of `SyntaxData` to 2 words.
    
    Also `SyntaxData.parent` now returns `SyntaxData` instead of `Syntax`.
    `SyntaxData` should not rely on `Syntax` API at all.

--- Sources/SwiftSyntax/Syntax.swift
+++ Sources/SwiftSyntax/Syntax.swift
@@ -143,7 +143,7 @@ public extension SyntaxProtocol {
 
   /// The index of this node in a `SyntaxChildren` collection.
   var index: SyntaxChildrenIndex {
-    return SyntaxChildrenIndex(self.data.absoluteRaw.info)
+    return SyntaxChildrenIndex(self.data.absoluteInfo)
   }
 
   /// Whether or not this node is a token one.
@@ -171,7 +171,7 @@ public extension SyntaxProtocol {
 
   /// The parent of this syntax node, or `nil` if this node is the root.
   var parent: Syntax? {
-    return data.parent
+    return data.parent.map(Syntax.init(_:))
   }
 
   /// The index of this node in the parent's children.

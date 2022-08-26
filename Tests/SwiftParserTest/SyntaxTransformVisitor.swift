5db2f10a5b8868b2c7cb1a15cc75b75f4216c4a9
zoecarver <z.zoelec2@gmail.com>
2022-08-26 16:19:14 -0700

    Add a new visitor: `SyntaxTransformVisitor`.
    This visitor returns a transformed Syntax node rather than exposing a pre-post visitor.

---
+++ Tests/SwiftParserTest/SyntaxTransformVisitor.swift
@@ -0,0 +1,97 @@
+//
+//  SyntaxTransformVisitor.swift
+//  
+//
+//  Created by Zoe Carver on 8/25/22.
+//
+
+import XCTest
+import SwiftParser
+import SwiftSyntax
+
+final class SyntaxTransformVisitorTest: XCTestCase {
+  public func testFunctionCounter() {
+    struct FuncCounter: SyntaxTransformVisitor {
+      public func visit(_ node: FunctionDeclSyntax) -> Int? {
+        return (visitChildren(node).first ?? 0) + 1
+      }
+    }
+    XCTAssertNoThrow(try {
+      let parsed = try Parser.parse(source: """
+          func foo() {
+            public func foo() {
+              func foo() {
+                /*Unknown token */0xG
+              }
+            }
+          }
+          """)
+      let count = FuncCounter().visit(parsed)
+      XCTAssertEqual(count, 3)
+    }())
+  }
+  
+  public func testStringify() {
+    struct Stringify: SyntaxTransformVisitor {
+      public func visitType(_ paramType: TypeSyntaxProtocol) -> String? {
+        if let id = SimpleTypeIdentifierSyntax(Syntax(fromProtocol: paramType)) {
+          return visit(id) ?? "<Unknown>"
+        }
+        if let arr = ArrayTypeSyntax(Syntax(fromProtocol: paramType)) {
+          return visit(arr) ?? "<Unknown>"
+        }
+        return "<Unknown>"
+      }
+      
+      public func visit(_ node: FunctionDeclSyntax) -> String? {
+        let argStrings = node.signature.input.parameterList
+          .compactMap { $0.type }
+          .compactMap(visitType)
+        
+        let resultString: String
+        if let out = node.signature.output {
+          resultString = visitType(out.returnType) ?? "<Unknown>"
+        } else {
+          resultString = "Void"
+        }
+        
+        return "(" + argStrings.joined(separator: ", ") + ") -> " + resultString
+      }
+      
+      public func visit(_ node: SimpleTypeIdentifierSyntax) -> String? {
+        node.name.text
+      }
+
+      public func visit(_ node: ArrayTypeSyntax) -> String? {
+        if let type = visitType(node.elementType) {
+          return "[" + type + "]"
+        }
+        return "[Unknown]"
+      }
+    }
+    XCTAssertNoThrow(try {
+      let parsed = try Parser.parse(source: """
+          func foo(a: Int, b: Foo, c: [Int]) -> Result {
+          }
+          """)
+      let stringified = Stringify().visit(parsed)
+      XCTAssertEqual(stringified, "(Int, Foo, [Int]) -> Result")
+    }())
+    XCTAssertNoThrow(try {
+      let parsed = try Parser.parse(source: """
+          func foo() {
+          }
+          """)
+      let stringified = Stringify().visit(parsed)
+      XCTAssertEqual(stringified, "() -> Void")
+    }())
+    XCTAssertNoThrow(try {
+      let parsed = try Parser.parse(source: """
+          func foo(a: Int) -> [Result] {
+          }
+          """)
+      let stringified = Stringify().visit(parsed)
+      XCTAssertEqual(stringified, "(Int) -> [Result]")
+    }())
+  }
+}

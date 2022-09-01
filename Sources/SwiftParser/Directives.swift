ef84ec3de71477cc07c2ab66acdb42cec1758faa
Alex Hoppen <ahoppen@apple.com>
2022-09-01 21:42:23 +0200

    Refactor loop for `#if` directive parsing
    This makes it easier to implement recovery in the future.

--- Sources/SwiftParser/Directives.swift
+++ Sources/SwiftParser/Directives.swift
@@ -68,21 +68,23 @@ extension Parser {
   ) -> RawIfConfigDeclSyntax {
     var clauses = [RawIfConfigClauseSyntax]()
     do {
-      var (unexpectedBeforePoundIf, poundIf) = self.expect(.poundIfKeyword)
-      repeat {
+      var firstIteration = true
+      var loopProgress = LoopProgressCondition()
+      while loopProgress.evaluate(self.currentToken),
+              let poundIf = self.consume(ifAny: firstIteration ? [.poundIfKeyword] : [.poundIfKeyword, .poundElseifKeyword, .poundElseKeyword]) {
+        firstIteration = false
         // Parse the condition.
+        let unexpectedBeforePoundIf: RawUnexpectedNodesSyntax?
         let condition: RawExprSyntax?
-        if let parsedElse = self.consume(if: .poundElseKeyword) {
+        switch poundIf.tokenKind {
+        case .poundIfKeyword, .poundElseifKeyword:
           unexpectedBeforePoundIf = nil
-          poundIf = parsedElse
+          condition = RawExprSyntax(self.parseSequenceExpression(.basic, forDirective: true))
+        case .poundElseKeyword:
+          unexpectedBeforePoundIf = nil
           condition = nil
-        } else if let poundElseif = self.consume(if: .poundElseifKeyword) {
-          unexpectedBeforePoundIf = nil
-          poundIf = poundElseif
-          condition = RawExprSyntax(self.parseSequenceExpression(.basic, forDirective: true))
-        } else {
-          assert(poundIf.tokenKind == .poundIfKeyword)
-          condition = RawExprSyntax(self.parseSequenceExpression(.basic, forDirective: true))
+        default:
+          preconditionFailure("The loop condition should guarantee that we are at one of these tokens")
         }
 
         var elements = [Element]()
@@ -101,7 +103,7 @@ extension Parser {
           condition: condition,
           elements: syntax(&self, elements),
           arena: self.arena))
-      } while self.at(any: [.poundElseifKeyword, .poundElseKeyword])
+      }
     }
 
     let (unexpectedBeforePoundEndIf, poundEndIf) = self.expect(.poundEndifKeyword)

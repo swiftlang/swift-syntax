e78df21366477c9e83be426715672c78812382b2
Doug Gregor <dgregor@apple.com>
2022-09-09 11:17:27 -0700

    Parallelize the self-test

--- Tests/SwiftParserTest/ParserTests.swift
+++ Tests/SwiftParserTest/ParserTests.swift
@@ -1,39 +1,49 @@
+import Dispatch
 import XCTest
 import SwiftSyntax
 import SwiftParser
 
 public class ParserTests: XCTestCase {
+  /// Run a single parse test.
+  func runParseTest(fileURL: URL, checkDiagnostics: Bool) throws {
+    let fileContents = try String(contentsOf: fileURL)
+    let parsed = try Parser.parse(source: fileContents)
+    AssertStringsEqualWithDiff("\(parsed)", fileContents)
+
+    if !checkDiagnostics {
+      return
+    }
+
+    let diagnostics = ParseDiagnosticsGenerator.diagnostics(for: parsed)
+    if !diagnostics.isEmpty {
+      var locationAndDiagnostics: [String] = []
+      let locationConverter = SourceLocationConverter(file: fileURL.lastPathComponent, tree: parsed)
+      for diag in diagnostics {
+        let location = diag.location(converter: locationConverter)
+        let message = diag.message
+        locationAndDiagnostics.append("\(location): \(message)")
+      }
+      XCTFail("""
+      Received the following diagnostics while parsing \(fileURL)
+      \(locationAndDiagnostics.joined(separator: "\n"))
+      """)
+    }
+  }
+
   /// Run parsr tests on all of the Swift files in the
-  func runParserTests(path: URL, checkDiagnostics: Bool) {
+  func runParserTests(name: String, path: URL, checkDiagnostics: Bool) {
     let fileURLs = FileManager.default
       .enumerator(at: path, includingPropertiesForKeys: nil)!
       .compactMap({ $0 as? URL })
       .filter({$0.pathExtension == "swift"})
-    for fileURL in fileURLs {
-      XCTAssertNoThrow(try {
-        let fileContents = try String(contentsOf: fileURL)
-        let parsed = try Parser.parse(source: fileContents)
-        AssertStringsEqualWithDiff("\(parsed)", fileContents)
 
-        if !checkDiagnostics {
-          return
-        }
-
-        let diagnostics = ParseDiagnosticsGenerator.diagnostics(for: parsed)
-        if !diagnostics.isEmpty {
-          var locationAndDiagnostics: [String] = []
-          let locationConverter = SourceLocationConverter(file: fileURL.lastPathComponent, tree: parsed)
-          for diag in diagnostics {
-            let location = diag.location(converter: locationConverter)
-            let message = diag.message
-            locationAndDiagnostics.append("\(location): \(message)")
-          }
-          XCTFail("""
-          Received the following diagnostics while parsing \(fileURL)
-          \(locationAndDiagnostics.joined(separator: "\n"))
-          """)
-        }
-      }())
+    DispatchQueue.concurrentPerform(iterations: fileURLs.count) { fileURLIndex in
+      let fileURL = fileURLs[fileURLIndex]
+      do {
+        try runParseTest(fileURL: fileURL, checkDiagnostics: checkDiagnostics)
+      } catch {
+        XCTFail("Test of \(fileURL) failed due to \(error)")
+      }
     }
   }
 
@@ -46,6 +56,8 @@ public class ParserTests: XCTestCase {
       .deletingLastPathComponent()
       .deletingLastPathComponent()
       .appendingPathComponent("Sources")
-    runParserTests(path: currentDir, checkDiagnostics: true)
+    runParserTests(
+      name: "Self-parse tests", path: currentDir, checkDiagnostics: true
+    )
   }
 }

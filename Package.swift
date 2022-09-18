d4c86abb514742a53f04fccacdbfc7c85aaf3526
Doug Gregor <dgregor@apple.com>
2022-09-18 10:30:52 -0700

    Move `swift_parser_consistencyCheck` into SwiftCompilerSupport.

--- Package.swift
+++ Package.swift
@@ -114,7 +114,6 @@ let package = Package(
       exclude: [
         "CMakeLists.txt",
         "README.md",
-        "SwiftParserCompilerSupport.h",
         "TypeAttribute.swift.gyb",
         "DeclarationModifier.swift.gyb",
         "DeclarationAttribute.swift.gyb",
--- Sources/SwiftCompilerSupport/ConsistencyCheck.swift
+++ Sources/SwiftCompilerSupport/ConsistencyCheck.swift
@@ -11,5 +11,77 @@
 //===----------------------------------------------------------------------===//
 
 import SwiftOperators
-import SwiftParser
-import SwiftSyntax
+@_spi(Testing) @_spi(RawSyntax) import SwiftParser
+@_spi(RawSyntax) import SwiftSyntax
+
+extension Syntax {
+  /// Whether this syntax node is or is enclosed within a #if.
+  fileprivate var isInIfConfig: Bool {
+    if self.is(IfConfigDeclSyntax.self) {
+      return true
+    }
+
+    return parent?.isInIfConfig ?? false
+  }
+}
+
+/// Entry point for the Swift compiler to use for consistency checking.
+///
+/// - Parameters:
+///   - bufferPtr: Pointer to the input buffer.
+///   - bufferLength: Length of the input buffer.
+///   - filename: The name of the source file, which is used only for diagnostics
+///   - flags: Flags that indicate what checks should be performed.
+///       0x01: Perform round-trip checking.
+///       0x02: Check for parser diagnostics.
+/// - Returns: 0 if all requested consistency checks passed, nonzero otherwise.
+@_cdecl("swift_parser_consistencyCheck")
+@_spi(SwiftCompiler)
+public func _parserConsistencyCheck(
+  bufferPtr: UnsafePointer<UInt8>, bufferLength: Int,
+  filename: UnsafePointer<UInt8>, flags: CUnsignedInt
+) -> CInt {
+  let buffer = UnsafeBufferPointer<UInt8>(
+    start: bufferPtr, count: bufferLength)
+  var parser = Parser(buffer)
+  return withExtendedLifetime(parser) { () -> CInt in
+    // Parse the source file
+    let rawSourceFile = parser.parseSourceFile()
+
+    // Round-trip test.
+    if flags & 0x01 != 0 {
+      if rawSourceFile.raw.syntaxTextBytes != [UInt8](buffer) {
+        print(
+          "\(String(cString: filename)): error: file failed to round-trip")
+        return 1
+      }
+    }
+
+    // Diagnostics test.
+    if flags & 0x02 != 0 {
+      var anyDiags = false
+
+      let sourceFile = Syntax(raw: rawSourceFile.raw).as(SourceFileSyntax.self)!
+
+      let diags = ParseDiagnosticsGenerator.diagnostics(
+        for: sourceFile)
+      for diag in diags {
+        // Skip over diagnostics within #if, because we don't know whether
+        // we are in an active region or not.
+        // FIXME: This heuristic could be improved.
+        if diag.node.isInIfConfig {
+          continue
+        }
+
+        print("\(diag.debugDescription)")
+        anyDiags = true
+      }
+
+      if anyDiags {
+        return 1
+      }
+    }
+
+    return 0
+  }
+}

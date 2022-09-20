d648ed17c7a6e085bf7368b5896337fef14d8d33
Alex Hoppen <ahoppen@apple.com>
2022-09-20 15:30:17 +0200

    Add a `nameForDiagnostics` to tokens

--- Tests/SwiftParserTest/Declarations.swift
+++ Tests/SwiftParserTest/Declarations.swift
@@ -653,7 +653,6 @@ final class DeclarationTests: XCTestCase {
       }
       """,
       diagnostics: [
-        // FIXME: This diagnostic should be more contextual
         DiagnosticSpec(message: "Expected '->' in return clause")
       ]
     )

466343dd61d9865ec09791c5461d5a0552e74516
Danny Mösch <danny.moesch@icloud.com>
2022-10-08 21:33:55 +0200

    Make examples runnable

---
+++ Examples/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "Examples",
+    platforms: [
+        .macOS(.v10_15),
+    ],
+    products: [
+        .executable(name: "AddOneToIntegerLiterals", targets: ["AddOneToIntegerLiterals"]),
+        .executable(name: "CodeGenerationUsingSwiftSyntaxBuilder", targets: ["CodeGenerationUsingSwiftSyntaxBuilder"]),
+    ],
+    dependencies: [
+        .package(path: "../"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "AddOneToIntegerLiterals",
+            dependencies: [
+                .product(name: "SwiftParser", package: "swift-syntax"),
+                .product(name: "SwiftSyntax", package: "swift-syntax"), 
+            ],
+            path: ".",
+            sources: ["AddOneToIntegerLiterals.swift"]
+        ),
+        .executableTarget(
+            name: "CodeGenerationUsingSwiftSyntaxBuilder",
+            dependencies: [
+                .product(name: "SwiftSyntaxBuilder", package: "swift-syntax"), 
+            ],
+            path: ".",
+            sources: ["CodeGenerationUsingSwiftSyntaxBuilder.swift"]
+        ),
+    ]
+)

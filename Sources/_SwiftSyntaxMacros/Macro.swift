2b092ab8801e268163b3fd6006dcf6990d8e73a7
Doug Gregor <dgregor@apple.com>
2022-10-23 21:31:06 -0700

    Extend macro protocols with information about the macro types.
    Macros are intended to have typed inputs and outputs, so express those
    via a signature and (optional) generic parameter clause to help express
    that signature. This puts the entirety of the macro's "declaration" in
    the conformance, so it can be queried by the macro system.

--- Sources/_SwiftSyntaxMacros/Macro.swift
+++ Sources/_SwiftSyntaxMacros/Macro.swift
@@ -9,9 +9,22 @@
 // See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 //===----------------------------------------------------------------------===//
+import SwiftSyntax
 
 /// Describes a macro.
 public protocol Macro {
   /// The name of this macro.
   static var name: String { get }
+
+  /// The generic signature to use when describing the type of this macro.
+  static var genericSignature: GenericParameterClauseSyntax? { get }
+
+  /// The type signature for this macro.
+  ///
+  /// A function type indicates a function-like macro (such as
+  /// `#colorLiteral(red: r, green: b, blue: b, alpha: a)`) that takes
+  /// arguments, whereas any other type indicates a value-like macro (such
+  /// as `#line`) that does not. This is a syntactic distinction, not a
+  /// semantic one.
+  static var signature: TypeSyntax { get }
 }

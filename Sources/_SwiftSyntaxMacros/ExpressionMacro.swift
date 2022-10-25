e3d26224030bcf525df7a10b20fd0a283ce1d30e
Richard Wei <rxwei@apple.com>
2022-10-25 02:18:22 -0700

    [Macros] Support user-defined macros as compiler plugins
    Make `Macro` refine `_CompilerPlugin` protocol when `_CompilerPluginSupport` exists. (`_CompilerPluginSupport` may not exist when ASTGen or SwiftCompilerSources is built with host tools.) This enables macros to talk to the compiler without an ABI-stable SwiftSyntax.

--- Sources/_SwiftSyntaxMacros/ExpressionMacro.swift
+++ Sources/_SwiftSyntaxMacros/ExpressionMacro.swift
@@ -11,6 +11,10 @@
 //===----------------------------------------------------------------------===//
 
 import SwiftSyntax
+import SwiftParser
+#if canImport(_CompilerPluginSupport)
+import _CompilerPluginSupport
+#endif
 
 /// Describes a macro that is explicitly expanded as an expression.
 public protocol ExpressionMacro: Macro {
@@ -20,3 +24,57 @@ public protocol ExpressionMacro: Macro {
     _ macro: MacroExpansionExprSyntax, in context: MacroEvaluationContext
   ) -> MacroResult<ExprSyntax>
 }
+
+#if canImport(_CompilerPluginSupport)
+extension ExpressionMacro {
+  public static func _kind() -> _CompilerPluginKind {
+    .expressionMacro
+  }
+
+  public static func _rewrite(
+    targetModuleName: UnsafePointer<UInt8>,
+    targetModuleNameCount: Int,
+    filePath: UnsafePointer<UInt8>,
+    filePathCount: Int,
+    sourceFileText: UnsafePointer<UInt8>,
+    sourceFileTextCount: Int,
+    localSourceText: UnsafePointer<UInt8>,
+    localSourceTextCount: Int
+  ) -> (UnsafePointer<UInt8>?, count: Int) {
+    let targetModuleNameBuffer = UnsafeBufferPointer(
+      start: filePath, count: targetModuleNameCount)
+    let targetModuleName = String(
+      decoding: targetModuleNameBuffer, as: UTF8.self)
+    let filePathBuffer = UnsafeBufferPointer(
+      start: filePath, count: filePathCount)
+    let filePath = String(decoding: filePathBuffer, as: UTF8.self)
+    let sourceFileTextBuffer = UnsafeBufferPointer(
+      start: sourceFileText, count: sourceFileTextCount)
+    let sourceFileString = String(decoding: sourceFileTextBuffer, as: UTF8.self)
+    let sourceFileSyntax = Parser.parse(source: sourceFileString)
+    let converter = SourceLocationConverter(
+      file: filePath, tree: sourceFileSyntax)
+    let context = MacroEvaluationContext(
+      moduleName: targetModuleName, sourceLocationConverter: converter)
+    let meePosition = AbsolutePosition(
+      utf8Offset: localSourceText.distance(to: localSourceText))
+    guard let meeStartToken = sourceFileSyntax.token(at: meePosition),
+          let mee = meeStartToken.parent?.as(MacroExpansionExprSyntax.self)
+    else {
+      fatalError("Unable to locate 'MacroExpansionExprSyntax'")
+    }
+
+    // Evaluate the macro.
+    let evalResult = apply(mee, in: context)
+
+    var resultString = "\(evalResult.rewritten)"
+    return resultString.withUTF8 { buffer in
+      let result = UnsafeMutableBufferPointer<UInt8>.allocate(
+        capacity: buffer.count + 1)
+      _ = result.initialize(from: buffer)
+      result[buffer.count] = 0
+      return (UnsafePointer(result.baseAddress), buffer.count)
+    }
+  }
+}
+#endif

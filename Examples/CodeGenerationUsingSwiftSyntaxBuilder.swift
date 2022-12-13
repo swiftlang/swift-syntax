fc50ed74ba9cc5e779046eccebd889e7ba02f52e
TiagoMaiaL <tiago.maia.lopes@gmail.com>
2022-12-13 19:58:55 -0300

    Fix comment indentation for SyntaxBuilder example

--- Examples/CodeGenerationUsingSwiftSyntaxBuilder.swift
+++ Examples/CodeGenerationUsingSwiftSyntaxBuilder.swift
@@ -14,24 +14,24 @@ extension String {
 ///
 /// ```
 /// struct Person {
-/// var lastName: String
-/// func withLastName(_ lastName: String) -> Person {
-///     var result = self
-///     result.lastName = lastName
-///     return result
-/// }
-/// var firstName: String
-/// func withFirstName(_ firstName: String) -> Person {
-///     var result = self
-///     result.firstName = firstName
-///     return result
-/// }
-/// var age: Int
-/// func withAge(_ age: Int) -> Person {
-///     var result = self
-///     result.age = age
-///     return result
-/// }
+///     var lastName: String
+///     func withLastName(_ lastName: String) -> Person {
+///         var result = self
+///         result.lastName = lastName
+///         return result
+///     }
+///     var firstName: String
+///     func withFirstName(_ firstName: String) -> Person {
+///         var result = self
+///         result.firstName = firstName
+///         return result
+///     }
+///     var age: Int
+///     func withAge(_ age: Int) -> Person {
+///         var result = self
+///         result.age = age
+///         return result
+///     }
 /// }
 /// ```
 ///

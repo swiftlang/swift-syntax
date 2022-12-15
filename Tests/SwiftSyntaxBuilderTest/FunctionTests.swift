6d6dc28d4d021e6b2a3386e906f0cfdc6909e0e8
stevapple <stevapple@icloud.com>
2022-12-15 19:46:49 +0800

    Adds a test case for simple function declaration formatting

--- Tests/SwiftSyntaxBuilderTest/FunctionTests.swift
+++ Tests/SwiftSyntaxBuilderTest/FunctionTests.swift
@@ -43,6 +43,20 @@ final class FunctionTests: XCTestCase {
     AssertBuildResult(buildable, "test(value1, p2: value2, value3, p4: value4, value5)")
   }
   
+  func testFunctionDeclBuilder() {
+    let builder = FunctionDecl("""
+      func test(_ p1: Int, p2: Int, _ p3: Int, p4: Int, _ p5: Int) -> Int {
+          return p1 + p2 + p3 + p4 + p5
+      }
+      """)
+    
+    AssertBuildResult(builder, """
+      func test(_ p1: Int, p2: Int, _ p3: Int, p4: Int, _ p5: Int) -> Int {
+          return p1 + p2 + p3 + p4 + p5
+      }
+      """)
+  }
+  
   func testMultilineFunctionParameterList() {
     let builder = FunctionDecl("""
       func test(

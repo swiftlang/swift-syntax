557bc8c3c87a0fe8779a542d5510a5611b4c679b
Holly Borla <hborla@apple.com>
2023-06-04 10:31:37 -0700

    Add new syntax nodes for initializes and accesses effects in init accessors.

--- CodeGeneration/Sources/SyntaxSupport/DeclNodes.swift
+++ CodeGeneration/Sources/SyntaxSupport/DeclNodes.swift
@@ -66,6 +66,74 @@ public let DECL_NODES: [Node] = [
   ),
 
   Node(
+    kind: .accessesEffect,
+    base: .syntax,
+    nameForDiagnostics: "accesses effect",
+    children: [
+      Child(
+        name: "AccessesKeyword",
+        kind: .token(choices: [.keyword(text: "accesses")])
+      ),
+      Child(
+        name: "LeftParen",
+        kind: .token(choices: [.token(tokenKind: "LeftParenToken")])
+      ),
+      Child(
+        name: "PropertyList",
+        kind: .collection(kind: .tupleExprElementList, collectionElementName: "Property"),
+        nameForDiagnostics: "properties"
+      ),
+      Child(
+        name: "RightParen",
+        kind: .token(choices: [.token(tokenKind: "RightParenToken")])
+      ),
+    ]
+  ),
+
+  Node(
+    kind: .initializesEffect,
+    base: .syntax,
+    nameForDiagnostics: "initializes effect",
+    children: [
+      Child(
+        name: "InitializesKeyword",
+        kind: .token(choices: [.keyword(text: "initializes")])
+      ),
+      Child(
+        name: "LeftParen",
+        kind: .token(choices: [.token(tokenKind: "LeftParenToken")])
+      ),
+      Child(
+        name: "PropertyList",
+        kind: .collection(kind: .tupleExprElementList, collectionElementName: "Property"),
+        nameForDiagnostics: "properties"
+      ),
+      Child(
+        name: "RightParen",
+        kind: .token(choices: [.token(tokenKind: "RightParenToken")])
+      ),
+    ]
+  ),
+
+  Node(
+    kind: .accessorInitEffects,
+    base: .syntax,
+    nameForDiagnostics: "init accessor effects",
+    children: [
+      Child(
+        name: "InitializesEffect",
+        kind: .node(kind: .initializesEffect),
+        isOptional: true
+      ),
+      Child(
+        name: "AccessesEffect",
+        kind: .node(kind: .accessesEffect),
+        isOptional: true
+      ),
+    ]
+  ),
+
+  Node(
     kind: .accessorDecl,
     base: .decl,
     nameForDiagnostics: "accessor",
@@ -110,6 +178,11 @@ public let DECL_NODES: [Node] = [
       Child(
         name: "EffectSpecifiers",
         kind: .node(kind: .accessorEffectSpecifiers),
+        isOptional: true
+      ),
+      Child(
+        name: "InitEffects",
+        kind: .node(kind: .accessorInitEffects),
         isOptional: true
       ),
       Child(

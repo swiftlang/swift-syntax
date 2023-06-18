f2c0ad0c8a7af65e642e7a535b0f638670f4d048
stevenwong <stevenhuang12@outlook.com>
2023-06-18 15:43:03 +0800

    Delete incremental parse related lit tests

--- Sources/lit-test-helper/main.swift
+++ Sources/lit-test-helper/main.swift
@@ -36,17 +36,9 @@ func printHelp() {
       -classify-syntax
             Parse the given source file (-source-file) and output it with
             tokens classified for syntax colouring.
-      -parse-incremental
-            Parse a pre-edit source file (-old-source-file) and incrementally
-            parse the post-edit source file (-source-file) that was the result
-            of applying the given edits (-incremental-edit).
       -roundtrip
             Parse the given source file (-source-file) and print it out using
             its syntax tree.
-      -verify-roundtrip
-            Parse the given source file (-source-file) and exit with a non-zero
-            exit code if printing the tree does not result in the original
-            source file.
       -print-tree
             Parse the given source file (-source-file) and output its syntax
             tree.
@@ -56,25 +48,6 @@ func printHelp() {
     Arguments:
       -source-file FILENAME
             The path to a Swift source file to parse
-      -old-source-file FILENAME
-            Path to the pre-edit source file to translate line:column edits into
-            the file's byte offsets.
-      -incremental-edit EDIT
-            An edit that was applied to reach the input file from the source
-            file that generated the old syntax tree in the format <start-line>:
-            <start-column>-<end-line>:<end-column>=<replacement> where start and
-            end are dfined in terms of the pre-edit file and <replacement> is
-            the string that shall replace the selected range. Can be passed
-            multiple times.
-      -reparse-region REGION
-            If specified, an error will be emitted if any part of the file
-            ouside of the reparse region gets parsed again. Can be passed
-            multiple times to specify multiple reparse regions. Reparse regions
-            are specified in the form <start-column>-<end-line>:<end-column> in
-            terms of the post-edit file.
-      -incremental-reuse-log FILENAME
-            Path to which a log should be written that describes all the nodes
-            reused during incremental parsing.
       -out FILENAME
             The file to which the source representation of the post-edit syntax
             tree shall be written.
@@ -86,155 +59,6 @@ func printHelp() {
             literal syntax
     """
   )
-}
-
-extension CommandLineArguments {
-  func getIncrementalEdits() throws -> [IncrementalEditSpec] {
-    let regex = try NSRegularExpression(
-      pattern: "([0-9]+):([0-9]+)-([0-9]+):([0-9]+)=(.*)"
-    )
-    var parsedEdits = [IncrementalEditSpec]()
-    let editArgs = try self.getValues("-incremental-edit")
-    for edit in editArgs {
-      guard
-        let match =
-          regex.firstMatch(
-            in: edit,
-            range: NSRange(edit.startIndex..., in: edit)
-          )
-      else {
-        throw CommandLineArguments.InvalidArgumentValueError(
-          argName: "-incremental-edit",
-          value: edit
-        )
-      }
-      let region = getSourceRegion(match, text: edit)
-      let replacement = match.match(at: 5, text: edit)
-      parsedEdits.append(
-        IncrementalEditSpec(
-          region: region,
-          replacement: replacement
-        )
-      )
-    }
-    return parsedEdits
-  }
-
-  func getReparseRegions() throws -> [SourceRegion] {
-    let regex = try NSRegularExpression(
-      pattern: "([0-9]+):([0-9]+)-([0-9]+):([0-9]+)"
-    )
-    var reparseRegions = [SourceRegion]()
-    let regionArgs = try self.getValues("-reparse-region")
-    for regionStr in regionArgs {
-      guard
-        let match =
-          regex.firstMatch(
-            in: regionStr,
-            range: NSRange(regionStr.startIndex..., in: regionStr)
-          )
-      else {
-        throw CommandLineArguments.InvalidArgumentValueError(
-          argName: "-reparse-region",
-          value: regionStr
-        )
-      }
-      let region = getSourceRegion(match, text: regionStr)
-      reparseRegions.append(region)
-    }
-    return reparseRegions
-  }
-
-  private func getSourceRegion(
-    _ match: NSTextCheckingResult,
-    text: String
-  ) -> SourceRegion {
-    let matchAsInt = { (i: Int) -> Int in
-      return Int(match.match(at: i, text: text))!
-    }
-
-    let startLine = matchAsInt(1)
-    let startColumn = matchAsInt(2)
-    let endLine = matchAsInt(3)
-    let endColumn = matchAsInt(4)
-    return SourceRegion(
-      startLine: startLine,
-      startColumn: startColumn,
-      endLine: endLine,
-      endColumn: endColumn
-    )
-  }
-}
-
-extension NSTextCheckingResult {
-  func match(at: Int, text: String) -> String {
-    let range = self.range(at: at)
-    let text = String(text[Range(range, in: text)!])
-    return text
-  }
-}
-
-struct ByteSourceRangeSet {
-  var ranges = [ByteSourceRange]()
-
-  mutating func addRange(_ range: ByteSourceRange) {
-    ranges.append(range)
-  }
-
-  func inverted(totalLength: Int) -> ByteSourceRangeSet {
-    var result = ByteSourceRangeSet()
-    var currentOffset = 0
-    for range in ranges {
-      precondition(
-        currentOffset <= range.offset,
-        "Ranges must be sorted in ascending order and not be overlapping"
-      )
-      if currentOffset < range.offset {
-        result.addRange(
-          ByteSourceRange(
-            offset: currentOffset,
-            length: range.offset - currentOffset
-          )
-        )
-      }
-      currentOffset = range.endOffset
-    }
-    if currentOffset < totalLength {
-      result.addRange(
-        ByteSourceRange(
-          offset: currentOffset,
-          length: totalLength - currentOffset
-        )
-      )
-    }
-
-    return result
-  }
-
-  func intersected(_ other: ByteSourceRangeSet) -> ByteSourceRangeSet {
-    var intersection = ByteSourceRangeSet()
-    for A in self.ranges {
-      for B in other.ranges {
-        let partialIntersection = A.intersected(B)
-        if !partialIntersection.isEmpty {
-          intersection.addRange(partialIntersection)
-        }
-      }
-    }
-    return intersection
-  }
-}
-
-struct SourceRegion {
-  let startLine: Int
-  let startColumn: Int
-  let endLine: Int
-  let endColumn: Int
-}
-
-struct IncrementalEditSpec {
-  let region: SourceRegion
-  let replacement: String
 }
 
 /// Rewrites a parsed tree with all constructed nodes.
@@ -271,123 +95,13 @@ func performClassifySyntax(args: CommandLineArguments) throws {
   }
 }
 
-/// Returns an array of UTF8 bytes offsets for each line.
-func getLineTable(_ text: String) -> [Int] {
-  return text.withCString { (p: UnsafePointer<Int8>) -> [Int] in
-    var lineOffsets = [Int]()
-    lineOffsets.append(0)
-    var idx = 0
-    while p[idx] != 0 {
-      if p[idx] == Int8(UnicodeScalar("\n").value) {
-        lineOffsets.append(idx + 1)
-      }
-      idx += 1
-    }
-    return lineOffsets
-  }
-}
-
-func getByteRange(
-  _ region: SourceRegion,
-  lineTable: [Int],
-  argName: String
-) throws -> ByteSourceRange {
-  if region.startLine - 1 >= lineTable.count {
-    throw CommandLineArguments.InvalidArgumentValueError(
-      argName: argName,
-      value: "startLine: \(region.startLine)"
-    )
-  }
-  if region.endLine - 1 >= lineTable.count {
-    throw CommandLineArguments.InvalidArgumentValueError(
-      argName: argName,
-      value: "endLine: \(region.endLine)"
-    )
-  }
-  let startOffset = lineTable[region.startLine - 1] + region.startColumn - 1
-  let endOffset = lineTable[region.endLine - 1] + region.endColumn - 1
-  let length = endOffset - startOffset
-  return ByteSourceRange(offset: startOffset, length: length)
-}
-
-func parseIncrementalEditArguments(
-  args: CommandLineArguments
-) throws -> [IncrementalEdit] {
-  var edits = [IncrementalEdit]()
-  let argEdits = try args.getIncrementalEdits()
-  let preEditURL =
-    URL(fileURLWithPath: try args.getRequired("-old-source-file"))
-  let text = try String(contentsOf: preEditURL)
-  let lineTable = getLineTable(text)
-  for argEdit in argEdits {
-    let range = try getByteRange(
-      argEdit.region,
-      lineTable: lineTable,
-      argName: "-incremental-edit"
-    )
-    let replacementLength = argEdit.replacement.utf8.count
-    edits.append(IncrementalEdit(range: range, replacementLength: replacementLength))
-  }
-  return edits
-}
-
-func performParseIncremental(args: CommandLineArguments) throws {
-  let preEditURL =
-    URL(fileURLWithPath: try args.getRequired("-old-source-file"))
-  let postEditURL = URL(fileURLWithPath: try args.getRequired("-source-file"))
-  let expectedReparseRegions = try args.getReparseRegions()
-
-  let preEditTree = try Parser.parse(preEditURL)
-  let edits = try parseIncrementalEditArguments(args: args)
-  let regionCollector = IncrementalParseReusedNodeCollector()
-  let editTransition = IncrementalParseTransition(
-    previousTree: preEditTree,
-    edits: try ConcurrentEdits(concurrent: edits),
-    reusedNodeDelegate: regionCollector
-  )
-
-  let postEditText = try String(contentsOf: postEditURL)
-  let postEditTree = Parser.parse(source: postEditText, parseTransition: editTransition)
-
-  let postTreeDump = postEditTree.description
-
-  if let outURL = args["-out"].map(URL.init(fileURLWithPath:)) {
-    try postTreeDump.write(to: outURL, atomically: false, encoding: .utf8)
-  } else {
-    print(postTreeDump)
-  }
-
-  let regions = regionCollector.rangeAndNodes.map { $0.0 }
-  if let reuseLogURL =
-    args["-incremental-reuse-log"].map(URL.init(fileURLWithPath:))
-  {
-    var log = ""
-    for region in regions {
-      log += "Reused \(region.offset) to \(region.endOffset)\n"
-    }
-    try log.write(to: reuseLogURL, atomically: false, encoding: .utf8)
-  }
-
-  if !expectedReparseRegions.isEmpty {
-    try verifyReusedRegions(
-      expectedReparseRegions: expectedReparseRegions,
-      reusedRegions: regions,
-      source: postEditText
-    )
-  }
-}
-
 enum TestingError: Error, CustomStringConvertible {
-  case reparsedRegionsVerificationFailed(ByteSourceRange)
   case classificationVerificationFailed(String, String)
   case readingSourceFileFailed(URL)
   case roundTripFailed
 
   public var description: String {
     switch self {
-    case .reparsedRegionsVerificationFailed(let range):
-      return "unexpectedly reparsed following region: (offset: \(range.offset),"
-        + " length:\(range.length))"
     case .classificationVerificationFailed(let parsed, let constructed):
       return """
         parsed vs constructed tree resulted in different classification output
@@ -404,52 +118,6 @@ enum TestingError: Error, CustomStringConvertible {
   }
 }
 
-func verifyReusedRegions(
-  expectedReparseRegions: [SourceRegion],
-  reusedRegions: [ByteSourceRange],
-  source text: String
-) throws {
-  let fileLength = text.utf8.count
-
-  // Compute the repared regions by inverting the reused regions
-  let reusedRanges = ByteSourceRangeSet(ranges: reusedRegions)
-  let reparsedRegions = reusedRanges.inverted(totalLength: fileLength)
-
-  // Same for expected reuse regions
-  let lineTable = getLineTable(text)
-  var expectedReparseRanges = ByteSourceRangeSet()
-  for region in expectedReparseRegions {
-    let range =
-      try getByteRange(region, lineTable: lineTable, argName: "-reparse-region")
-    expectedReparseRanges.addRange(range)
-  }
-  let expectedReuseRegions =
-    expectedReparseRanges.inverted(totalLength: fileLength)
-
-  // Intersect the reparsed regions with the expected reuse regions to get
-  // regions that should not have been reparsed
-  let unexpectedReparseRegions =
-    reparsedRegions.intersected(expectedReuseRegions)
-
-  for reparseRange in unexpectedReparseRegions.ranges {
-    // To improve the ergonomics when writing tests we do not want to complain
-    // about reparsed whitespaces.
-    let utf8 = text.utf8
-    let begin = utf8.index(utf8.startIndex, offsetBy: reparseRange.offset)
-    let end = utf8.index(begin, offsetBy: reparseRange.length)
-    let rangeStr = String(utf8[begin..<end])!
-    let whitespaceOnlyRegex = try NSRegularExpression(pattern: "^[ \t\r\n]*$")
-    let match = whitespaceOnlyRegex.firstMatch(
-      in: rangeStr,
-      range: NSRange(rangeStr.startIndex..., in: rangeStr)
-    )
-    if match != nil {
-      continue
-    }
-    throw TestingError.reparsedRegionsVerificationFailed(reparseRange)
-  }
-}
-
 func performRoundtrip(args: CommandLineArguments) throws {
   let sourceURL = URL(fileURLWithPath: try args.getRequired("-source-file"))
   let tree = try Parser.parse(sourceURL)
@@ -459,18 +127,6 @@ func performRoundtrip(args: CommandLineArguments) throws {
     try treeText.write(to: outURL, atomically: false, encoding: .utf8)
   } else {
     print(treeText)
-  }
-}
-
-func performVerifyRoundtrip(args: CommandLineArguments) throws {
-  let sourceURL = URL(fileURLWithPath: try args.getRequired("-source-file"))
-  guard let source = try String(data: Data(contentsOf: sourceURL), encoding: .utf8) else {
-    throw TestingError.readingSourceFileFailed(sourceURL)
-  }
-
-  let tree = Parser.parse(source: source)
-  if tree.description != source {
-    throw TestingError.roundTripFailed
   }
 }
 
@@ -510,12 +166,8 @@ do {
 
   if args.has("-classify-syntax") {
     try performClassifySyntax(args: args)
-  } else if args.has("-parse-incremental") {
-    try performParseIncremental(args: args)
   } else if args.has("-roundtrip") {
     try performRoundtrip(args: args)
-  } else if args.has("-verify-roundtrip") {
-    try performVerifyRoundtrip(args: args)
   } else if args.has("-print-tree") {
     try printSyntaxTree(args: args)
   } else if args.has("-help") {
--- lit_tests/incrParse/multi-edit-mapping.swift
+++
@@ -1,7 +0,0 @@
-// RUN: %empty-directory(%t)
-// RUN: %validate-incrparse %s --test-case MULTI
-
-// https://github.com/apple/swift-syntax/issues/1045
-// XFAIL: *
-
-let one: Int;<reparse MULTI>let two: Int; let three: Int; <<MULTI<|||                      >>><<MULTI<|||   >>>let found: Int;</reparse MULTI>let five: Int;

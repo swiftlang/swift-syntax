60ac579a356856a4501c3ad8bf2d02be8c2a462c
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2023-06-20 10:34:46 +0900

    Use `continue` instead of copy-pasted code.

--- Sources/SwiftSyntaxBuilder/ConvenienceInitializers.swift
+++ Sources/SwiftSyntaxBuilder/ConvenienceInitializers.swift
@@ -288,8 +288,7 @@ extension StringLiteralExprSyntax {
         consecutivePounds += 1
         maxPounds = max(maxPounds, consecutivePounds)
       case (true, "\""), (true, "\\"):
-        countingPounds = true
-        requiresEscaping = true
+        continue
       case (true, _):
         countingPounds = false
         consecutivePounds = 0

c2c93ad830b979b82aeaa10c62e65c2ed84e5bc7
Holly Borla <hborla@apple.com>
2023-06-28 14:59:46 -0700

    [Macros] Fix typos in comments and documentation for extension macros.
    (cherry picked from commit 3e2c782f0ef59dc28497932d4de0fb69d0413f23)

--- Sources/SwiftSyntaxMacros/MacroProtocols/ExtensionMacro.swift
+++ Sources/SwiftSyntaxMacros/MacroProtocols/ExtensionMacro.swift
@@ -12,22 +12,23 @@
 
 import SwiftSyntax
 
-/// Describes a macro that can add conformances to the declaration it's
+/// Describes a macro that can add extensions to the declaration it's
 /// attached to.
 public protocol ExtensionMacro: AttachedMacro {
-  /// Expand an attached conformance macro to produce a set of conformances.
+  /// Expand an attached extension macro to produce a set of extensions.
   ///
   /// - Parameters:
   ///   - node: The custom attribute describing the attached macro.
   ///   - declaration: The declaration the macro attribute is attached to.
+  ///   - type: The type to provide extensions of.
   ///   - context: The context in which to perform the macro expansion.
   ///
-  /// - Returns: the set of `(type, where-clause?)` pairs that each provide the
-  ///   protocol type to which the declared type conforms, along with
-  ///   an optional where clause.
+  /// - Returns: the set of extension declarations introduced by the macro,
+  ///   which are always inserted at top-level scope. Each extension must extend
+  ///   the `type` parameter.
   static func expansion(
     of node: AttributeSyntax,
-    attachedTo decl: some DeclGroupSyntax,
+    attachedTo declaration: some DeclGroupSyntax,
     providingExtensionsOf type: some TypeSyntaxProtocol,
     in context: some MacroExpansionContext
   ) throws -> [ExtensionDeclSyntax]

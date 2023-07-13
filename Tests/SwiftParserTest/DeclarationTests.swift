8d6aed296c36e805fb74892eb66184550d8ee5c9
Alex Hoppen <ahoppen@apple.com>
2023-07-13 12:56:00 +0200

    Support parsing of `borrowing get`
    rdar://112168704

--- Tests/SwiftParserTest/DeclarationTests.swift
+++ Tests/SwiftParserTest/DeclarationTests.swift
@@ -2031,4 +2031,16 @@ final class DeclarationTests: XCTestCase {
         """
     )
   }
+
+  func testBorrowingGetAccessor() {
+    assertParse(
+      """
+      struct Foo {
+        var x: Int {
+          borrowing get {}
+        }
+      }
+      """
+    )
+  }
 }

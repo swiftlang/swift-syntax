a63c29240c62ce3418bd446221246cb3c4e1f68c
Alex Hoppen <ahoppen@apple.com>
2023-08-11 10:36:12 -0700

    Add a version indicator macro to indicate the swift-syntax version a client is building against
    All swift-syntax versions ≥ 509 will include a module `SwiftSyntax509` and all swift-syntax versions ≥ 510 will include both `SwiftSyntax509` and `SwiftSyntax510`. This way clients can check which version of swift-syntax they are building against using e.g.
    
    ```swift
    #if canImport(SwiftSyntax510)
    // code specific to swift-syntax version >= 510
    #else
    // code for swift-syntax < 510
    #endif
    ```

---
+++ Sources/SwiftSyntax509/Empty.swift
@@ -0,0 +1,3 @@
+// The SwiftSyntax509 module is intentionally empty.
+// It serves as an indicator which version of swift-syntax a package is building against.
+// See the 'Macro Versioning.md' document for more details.

826055a462557315b5e359e6a31946badae654d8
Mateusz Bąk <44930823+Matejkob@users.noreply.github.com>
2023-08-13 12:37:40 +0200

    Move macro examples code to repo as-is
    - Code was sourced from Doug's repo and moved without any alterations except for formatting changes.
    - Original target names have been updated to more descriptive ones.
    - References in macros to target names have been updated accordingly.

--- Examples/Package.swift
+++ Examples/Package.swift
@@ -1,6 +1,7 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 
 import PackageDescription
+import CompilerPluginSupport
 
 let package = Package(
   name: "Examples",
@@ -37,6 +38,37 @@ let package = Package(
         .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
         .product(name: "SwiftDiagnostics", package: "swift-syntax"),
       ]
+    ),
+    .macro(
+      name: "MacroExamplesImplementation",
+      dependencies: [
+        .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+        .product(name: "SwiftSyntax", package: "swift-syntax"),
+        .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+        .product(name: "SwiftDiagnostics", package: "swift-syntax"),
+      ],
+      path: "Sources/MacroExamples/Implementation"
+    ),
+    .target(
+      name: "MacroExamplesInterface",
+      dependencies: [
+        "MacroExamplesImplementation"
+      ],
+      path: "Sources/MacroExamples/Interface"
+    ),
+    .executableTarget(
+      name: "MacroExamplesPlayground",
+      dependencies: [
+        "MacroExamplesInterface"
+      ],
+      path: "Sources/MacroExamples/Playground"
+    ),
+    .testTarget(
+      name: "MacroExamplesImplementationTests",
+      dependencies: [
+        "MacroExamplesImplementation"
+      ],
+      path: "Tests/MacroExamples/Implementation"
     ),
   ]
 )

c7f3b83292c47e454b830afa01c0b64cdca38caa
Alex Hoppen <ahoppen@apple.com>
2023-11-07 14:28:18 -0800

    Make CodeGeneration depend on swift-syntax 509
    https://github.com/apple/swift-syntax/pull/2239 was meant to make Generation depend on swift-syntax 509 but it didn’t actually flip the switch. Do so now.

--- CodeGeneration/Package.swift
+++ CodeGeneration/Package.swift
@@ -12,17 +12,8 @@ let package = Package(
     .executable(name: "generate-swift-syntax", targets: ["generate-swift-syntax"])
   ],
   dependencies: [
-    // This directory is a standalone package that uses swift-syntax from the
-    // outer directory.
-    // If you are making significant changs to `CodeGeneration` locally and want
-    // to avoid breaking the build of `CodeGeneration` itself by generating new
-    // files in the parent swift-syntax package, it is encouraged to change the
-    // dependency to the following. That way `CodeGeneration` has its own
-    // checkout of swift-syntax that is unaffected by the newly generated files.
-    // Be sure to revert the change before committing your changes.
-    //
-    // .package(url: "https://github.com/apple/swift-syntax", branch: "main")
-    .package(path: "..")
+    .package(url: "https://github.com/apple/swift-syntax", from: "509.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
   ],
   targets: [
     .executableTarget(
@@ -63,14 +54,3 @@ let package = Package(
     ),
   ]
 )
-
-if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
-  // Building standalone.
-  package.dependencies += [
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2")
-  ]
-} else {
-  package.dependencies += [
-    .package(path: "../../swift-argument-parser")
-  ]
-}

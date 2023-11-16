7ac533605dca605c6e1ba1b68439188b6acd9e7d
KaitoMuraoka <kaitoboshi@icloud.com>
2023-11-16 23:06:07 +0900

    fix: Formatter.step was adapted to Xcode15.

--- Sources/SwiftSyntax/Documentation.docc/Resources/Package.step1.swift
+++ Sources/SwiftSyntax/Documentation.docc/Resources/Package.step1.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,22 +6,17 @@ import PackageDescription
 let package = Package(
   name: "importformatter",
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    // Products define the executables and libraries a package produces, making them visible to other packages.
     .library(
       name: "importformatter",
       targets: ["importformatter"]
     )
   ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-  ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
     .target(
-      name: "importformatter",
-      dependencies: []
+      name: "importformatter"
     ),
     .testTarget(
       name: "importformatterTests",

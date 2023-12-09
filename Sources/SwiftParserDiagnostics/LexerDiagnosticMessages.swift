0ed5a1c5f2e99c64d968a0ffacbef648f9a6dec0
Alex Hoppen <ahoppen@apple.com>
2023-12-08 18:21:58 -0800

    Fix incorrect copyright headers

--- Sources/SwiftParserDiagnostics/LexerDiagnosticMessages.swift
+++ Sources/SwiftParserDiagnostics/LexerDiagnosticMessages.swift
@@ -1,4 +1,4 @@
-//===--- LexerDiagnosticMessages.swift ------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // This source file is part of the Swift.org open source project
 //

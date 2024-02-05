d4dcba0339ce72b2f29e4c6f96e8dc45549c0e60
Mateusz Bąk <bakmatthew@icloud.com>
2024-02-05 11:52:44 +0100

    Update file headers: Change year from 2023 to 2024

--- Sources/SwiftSyntaxMacros/MacroProtocols/PeerMacro.swift
+++ Sources/SwiftSyntaxMacros/MacroProtocols/PeerMacro.swift
@@ -1,6 +1,6 @@
 //===----------------------------------------------------------------------===//
 //
-// Copyright (c) 2014 - 2023 Apple Inc. and the Swift project authors
+// Copyright (c) 2014 - 2024 Apple Inc. and the Swift project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
 // See https://swift.org/LICENSE.txt for license information

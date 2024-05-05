85e03749d0d82561b3c37391e16d0d01c9808a98
Kabir Oberai <oberai.kabir@gmail.com>
2024-05-05 13:57:49 -0500

    Change wasm versioning approach

--- Sources/_SwiftSyntaxCShims/wasm_support.c
+++ Sources/_SwiftSyntaxCShims/wasm_support.c
@@ -12,25 +12,10 @@
 
 #if defined(__wasm32__)
 
-// uint32_t
-#define SWIFT_WASM_MACRO_ABI 1
-
-#define _STR(X) #X
-#define STR(X) _STR(X)
-
-// Add a `swift_wasm_macro_abi` section to the generated Wasm file
-// to allow the runner to check our ABI version and act accordingly.
-//
-// LLVM has special-cased handling to map .custom_section.foo to
-// Wasm Custom Section "foo". this must be a metadata section rather
-// than a data section so we can't use __attribute__((section)) for it.
-// See: https://reviews.llvm.org/D43097
-__asm__("\t.section .custom_section.swift_wasm_macro_abi,\"\",@\n\t.4byte " STR(SWIFT_WASM_MACRO_ABI) "\n");
-
 // defined in CompilerPluginMessageHandler.swift
 void _swift_wasm_macro_pump(void);
 
-__attribute__((export_name("swift_wasm_macro_pump")))
+__attribute__((export_name("swift_wasm_macro_v1_pump")))
 void swift_wasm_macro_pump(void) {
     _swift_wasm_macro_pump();
 }

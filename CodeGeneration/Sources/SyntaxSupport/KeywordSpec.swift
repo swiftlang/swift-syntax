c7c2354840b97e9081d02049439299af6aebff22
Jacob Bartlett <jacob@lapse.com>
2024-10-02 16:37:39 +0100

    Removed bullshitty keywords

--- CodeGeneration/Sources/SyntaxSupport/KeywordSpec.swift
+++ CodeGeneration/Sources/SyntaxSupport/KeywordSpec.swift
@@ -78,160 +78,55 @@ public func keywordsByLength() -> [(Int, [KeywordSpec])] {
 
 public enum Keyword: CaseIterable {
   // Please keep these sorted alphabetically
-
-  case __consuming
-  case __owned
-  case __setter_access
-  case __shared
-  case _alignment
-  case _backDeploy
-  case _borrow
-  case _borrowing
-  case _BridgeObject
-  case _cdecl
-  case _Class
-  case _compilerInitialized
-  case _const
-  case _consuming
-  case _documentation
-  case _dynamicReplacement
-  case _effects
-  case _expose
-  case _forward
-  case _implements
-  case _linear
-  case _local
-  case _modify
-  case _move
-  case _mutating
-  case _NativeClass
-  case _NativeRefCountedObject
-  case _noMetadata
-  case _nonSendable
-  case _objcImplementation
-  case _objcRuntimeName
-  case _opaqueReturnTypeOf
-  case _optimize
-  case _originallyDefinedIn
-  case _PackageDescription
-  case _private
-  case _projectedValueProperty
-  case _read
-  case _RefCountedObject
-  case _semantics
-  case _specialize
-  case _spi
-  case _spi_available
-  case _swift_native_objc_runtime_base
-  case _Trivial
-  case _TrivialAtMost
-  case _TrivialStride
-  case _typeEraser
-  case _unavailableFromAsync
-  case _underlyingVersion
-  case _UnknownLayout
-  case _version
-  case accesses
   case actor
-  case addressWithNativeOwner
-  case addressWithOwner
   case any
   case `Any`
   case `as`
   case assignment
   case `associatedtype`
-  case associativity
   case async
-  case attached
-  case autoclosure
-  case availability
   case available
   case await
-  case backDeployed
-  case before
-  case block
-  case borrowing
   case `break`
-  case canImport
   case `case`
-  case `catch`
   case `class`
-  case compiler
-  case consume
-  case copy
-  case consuming
   case `continue`
   case convenience
-  case convention
-  case cType
-  case `default`
   case `defer`
   case `deinit`
-  case dependsOn
-  case deprecated
-  case derivative
   case didSet
-  case differentiable
-  case distributed
   case `do`
   case dynamic
-  case each
   case `else`
   case `enum`
   case escaping
-  case exclusivity
-  case exported
   case `extension`
   case `fallthrough`
   case `false`
-  case file
   case `fileprivate`
   case final
   case `for`
-  case discard
-  case forward
   case `func`
-  case freestanding
   case get
   case `guard`
-  case higherThan
   case `if`
   case `import`
   case `in`
-  case indirect
   case infix
   case `init`
-  case initializes
   case inline
   case `inout`
   case `internal`
-  case introduced
   case `is`
   case isolated
-  case kind
   case lazy
-  case left
   case `let`
-  case line
-  case linear
-  case lowerThan
-  case macro
-  case message
-  case metadata
-  case modify
-  case module
-  case mutableAddressWithNativeOwner
-  case mutableAddressWithOwner
   case mutating
   case `nil`
-  case noasync
-  case noDerivative
   case noescape
   case none
   case nonisolated
-  case nonmutating
   case objc
-  case obsoleted
   case of
   case open
   case `operator`
@@ -240,50 +135,32 @@ public enum Keyword: CaseIterable {
   case package
   case postfix
   case `precedencegroup`
-  case preconcurrency
   case prefix
   case `private`
-  case `Protocol`
   case `protocol`
   case `public`
-  case read
-  case reasync
-  case renamed
   case `repeat`
   case required
   case `rethrows`
-  case retroactive
   case `return`
-  case reverse
-  case right
-  case safe
-  case scoped
   case `self`
-  case sending
   case `Self`
   case Sendable
   case set
   case some
-  case sourceFile
-  case spi
-  case spiModule
   case `static`
   case `struct`
   case `subscript`
   case `super`
-  case swift
   case `switch`
-  case target
   case then
   case `throw`
   case `throws`
-  case transpose
   case `true`
   case `try`
   case `Type`
   case `typealias`
   case unavailable
-  case unchecked
   case unowned
   case unsafe
   case unsafeAddress
@@ -295,8 +172,6 @@ public enum Keyword: CaseIterable {
   case `while`
   case willSet
   case witness_method
-  case wrt
-  case yield
 
   public var spec: KeywordSpec {
     switch self {

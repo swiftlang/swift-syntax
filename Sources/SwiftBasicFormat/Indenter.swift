a26f71d8dc7918d59c9880e0fcfa817d4b02a454
Jed Fox <jedfox@apple.com>
2024-10-26 15:59:29 -0700

    Copyright headers

--- Sources/SwiftBasicFormat/Indenter.swift
+++ Sources/SwiftBasicFormat/Indenter.swift
@@ -1,9 +1,14 @@
+//===----------------------------------------------------------------------===//
 //
-//  Indenter.swift
-//  swift-syntax
+// This source file is part of the Swift.org open source project
 //
-//  Created by Jed Fox on 9/8/24.
+// Copyright (c) 2024 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
 
 #if swift(>=6)
 public import SwiftSyntax

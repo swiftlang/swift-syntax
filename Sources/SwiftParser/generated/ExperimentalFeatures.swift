3112311fd074b51920c0078742675f1617a6fc70
Alejandro Alonso <alejandro_alonso@apple.com>
2025-03-26 10:59:35 -0700

    Update ExperimentalFeatures.swift

--- Sources/SwiftParser/generated/ExperimentalFeatures.swift
+++ Sources/SwiftParser/generated/ExperimentalFeatures.swift
@@ -53,7 +53,7 @@ extension Parser.ExperimentalFeatures {
   public static let oldOwnershipOperatorSpellings = Self (rawValue: 1 << 8)
 
   /// Whether to enable the parsing of sugar type for InlineArray.
-  public static let inlineArrayTypeSugar = Self (rawValue: 1 << 10)
+  public static let inlineArrayTypeSugar = Self (rawValue: 1 << 9)
 
   /// Creates a new value representing the experimental feature with the
   /// given name, or returns nil if the name is not recognized.

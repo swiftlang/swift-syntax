e11e519ec3b77523258c4dbc30a4ac1f28daa0bc
Aviral Goel <goel.aviral@gmail.com>
2026-09-16 15:55:07 -0700

    Allow a MessageID's domain and id to be read
    Make `MessageID`'s `domain` and `id` fields public. This enables clients to read
    them for displaying and logging. This modification does not change any behavior.

--- Sources/SwiftDiagnostics/Message.swift
+++ Sources/SwiftDiagnostics/Message.swift
@@ -17,8 +17,8 @@
 /// same wording. Eg. it’s possible that the message contains more context when
 /// available.
 public struct MessageID: Hashable, Sendable {
-  private let domain: String
-  private let id: String
+  public let domain: String
+  public let id: String
 
   public init(domain: String, id: String) {
     self.domain = domain
